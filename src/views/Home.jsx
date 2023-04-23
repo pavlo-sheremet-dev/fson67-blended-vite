be3db12853d9248d2c3736dcbc493dc1718015db
Daria-Misko <miskodaria.mail@gmail.com>
2023-04-23 11:46:58 +0300

    task 1

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,38 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { CocktailsList } from "../components/CocktailsList";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { useEffect, useState } from "react";
+import { getTrendingCocktails } from "../api/cocktail-service";
 
 export const Home = () => {
-  return (
-    <>
-      <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
-          Trending cocktails
-        </h1>
+	const [cocktails, setCocktails] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-        {/* <CocktailsList cocktails={cocktails} /> */}
-      </Section>
-    </>
-  );
+	useEffect(() => {
+		const asincWrapper = async () => {
+			setIsLoading(true);
+			try {
+				const responce = await getTrendingCocktails();
+				setCocktails(responce);
+			} catch (error) {
+				console.log(error.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		asincWrapper();
+	}, []);
+
+	return (
+		<>
+			<Section>
+				<h1 className="text-center font-black text-gray-700 text-4xl mb-10">
+					Trending cocktails
+				</h1>
+
+				<CocktailsList cocktails={cocktails} />
+			</Section>
+			{isLoading && <Loader />}
+		</>
+	);
 };
